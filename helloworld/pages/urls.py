ee0a27695b0b225ade14d6886e5579f5e9e135bd
Samuel Villa Valencia <svillav4@eafit.edu.co>
2025-02-12 21:41:46 -0500

    tutorial 2 + bonus

--- helloworld/pages/urls.py
+++ helloworld/pages/urls.py
@@ -1,12 +1,18 @@
-from django.urls import path 
+from django.urls import path
 from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView, ProductSuccessView
 
-urlpatterns = [ 
+urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
     path('contact/', ContactPageView.as_view(), name='contact'),
-    path('products/', ProductIndexView.as_view(), name='index'), 
-    path('products/create', ProductCreateView.as_view(), name='product_create'),
-    path('products/success', ProductSuccessView.as_view(), name='product_success'),
+    path('products/', ProductIndexView.as_view(), name='index'),
+    path(
+        'products/create',
+        ProductCreateView.as_view(),
+        name='product_create'),
+    path(
+        'products/success',
+        ProductSuccessView.as_view(),
+        name='product_success'),
     path('products/<str:id>', ProductShowView.as_view(), name='show'),
-] 
+]
--- helloworld/pages/views.py
+++ helloworld/pages/views.py
@@ -3,58 +3,68 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import TemplateView, ListView
 from django.views import View
 from django.urls import reverse
-from django import forms 
+from django import forms
 from .models import Product
 
-class HomePageView(TemplateView): 
+
+class HomePageView(TemplateView):
     template_name = 'pages/home.html'
 
-class AboutPageView(TemplateView): 
-    template_name = 'pages/about.html' 
-    def get_context_data(self, **kwargs): 
-        context = super().get_context_data(**kwargs) 
-        context.update({ 
-            "title": "About us - Online Store", 
-            "subtitle": "About us", 
-            "description": "This is an about page ...", 
-            "author": "Developed by: Samuel Villa", 
-        }) 
-        return context 
-    
-class ContactPageView(TemplateView): 
-    template_name = 'pages/contact.html' 
-    def get_context_data(self, **kwargs): 
-        context = super().get_context_data(**kwargs) 
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "About us - Online Store",
+            "subtitle": "About us",
+            "description": "This is an about page ...",
+            "author": "Developed by: Samuel Villa",
+        })
+        return context
+
+
+class ContactPageView(TemplateView):
+    template_name = 'pages/contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         context.update({
             "title": "Contact information (Fake)",
-            "email": "example@example.com", 
-            "address": "Calle 51 # 81z - 99", 
-            "phoneNumber": "+57 3174535678", 
-        }) 
-        return context 
-    
-'''class Product: 
-    products = [ 
-        {"id":"1", "name":"TV", "description":"Best TV", "price":500}, 
-        {"id":"2", "name":"iPhone", "description":"Best iPhone", "price":1000}, 
-        {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price":100}, 
+            "email": "example@example.com",
+            "address": "Calle 51 # 81z - 99",
+            "phoneNumber": "+57 3174535678",
+        })
+        return context
+
+
+'''class Product:
+    products = [
+        {"id":"1", "name":"TV", "description":"Best TV", "price":500},
+        {"id":"2", "name":"iPhone", "description":"Best iPhone", "price":1000},
+        {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price":100},
         {"id":"4", "name":"Glasses", "description":"Best Glasses", "price":20},
-    ] 
+    ]
 '''
 
-class ProductIndexView(View): 
-    template_name = 'products/index.html' 
-    def get(self, request): 
-        viewData = {} 
-        viewData["title"] = "Products - Online Store" 
-        viewData["subtitle"] =  "List of products" 
-        viewData["products"] = Product.objects.all() 
- 
-        return render(request, self.template_name, viewData) 
- 
-class ProductShowView(View): 
-    template_name = 'products/show.html' 
-    def get(self, request, id):  
+
+class ProductIndexView(View):
+    template_name = 'products/index.html'
+
+    def get(self, request):
+        viewData = {}
+        viewData["title"] = "Products - Online Store"
+        viewData["subtitle"] = "List of products"
+        viewData["products"] = Product.objects.all()
+
+        return render(request, self.template_name, viewData)
+
+
+class ProductShowView(View):
+    template_name = 'products/show.html'
+
+    def get(self, request, id):
         try:
             product_id = int(id)
             if product_id < 1:
@@ -63,58 +73,61 @@ class ProductShowView(View):
 
         except (ValueError, IndexError):
             return HttpResponseRedirect(reverse("home"))
-           
-        viewData = {} 
-        product = get_object_or_404(Product, pk=product_id)
-        viewData["title"] = product.name + " - Online Store" 
-        viewData["subtitle"] = product.name + " - Product information" 
-        viewData["product"] = product
-        
-        return render(request, self.template_name, viewData)     
 
-class ProductForm(forms.ModelForm): 
-    class Meta: 
-        model = Product 
-        fields = ['name', 'price'] 
+        viewData = {}
+        product = get_object_or_404(Product, pk=product_id)
+        viewData["title"] = product.name + " - Online Store"
+        viewData["subtitle"] = product.name + " - Product information"
+        viewData["product"] = product
+
+        return render(request, self.template_name, viewData)
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'price']
 
     def clean_price(self):
         price = self.cleaned_data.get("price")
         if price <= 0:
             raise forms.ValidationError("The price must be greater than 0.")
-        return price 
- 
-class ProductCreateView(View): 
-    template_name = 'products/create.html' 
- 
-    def get(self, request): 
-        form = ProductForm() 
-        viewData = {} 
-        viewData["title"] = "Create product" 
-        viewData["form"] = form 
-        return render(request, self.template_name, viewData) 
- 
-    def post(self, request): 
-        form = ProductForm(request.POST) 
-        if form.is_valid(): 
+        return price
+
+
+class ProductCreateView(View):
+    template_name = 'products/create.html'
+
+    def get(self, request):
+        form = ProductForm()
+        viewData = {}
+        viewData["title"] = "Create product"
+        viewData["form"] = form
+        return render(request, self.template_name, viewData)
+
+    def post(self, request):
+        form = ProductForm(request.POST)
+        if form.is_valid():
             form.save()
             return redirect('product_success')
-        else: 
-            viewData = {} 
-            viewData["title"] = "Create product" 
-            viewData["form"] = form 
+        else:
+            viewData = {}
+            viewData["title"] = "Create product"
+            viewData["form"] = form
             return render(request, self.template_name, viewData)
+
 
 class ProductSuccessView(TemplateView):
     template_name = 'products/success.html'
 
-class ProductListView(ListView): 
-    model = Product 
-    template_name = 'product_list.html' 
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product_list.html'
     context_object_name = 'products'
- 
-    def get_context_data(self, **kwargs): 
-        context = super().get_context_data(**kwargs) 
-        context['title'] = 'Products - Online Store' 
-        context['subtitle'] = 'List of products' 
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Products - Online Store'
+        context['subtitle'] = 'List of products'
         return context
-    
