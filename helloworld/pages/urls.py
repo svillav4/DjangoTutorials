37bcada6a003435c97fdc57a0f2f86c8e5a15997
TheSamVil <svillav4@eafit.edu.co>
2025-02-17 11:38:02 -0500

    tutorial 03-A finished

--- helloworld/pages/urls.py
+++ helloworld/pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView, ProductSuccessView
+from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView, ProductSuccessView, CartView, CartRemoveAllView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
@@ -15,4 +15,7 @@ urlpatterns = [
         ProductSuccessView.as_view(),
         name='product_success'),
     path('products/<str:id>', ProductShowView.as_view(), name='show'),
+    path('cart/', CartView.as_view(), name='cart_index'), 
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'), 
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
 ]
